a8fac6920155e6a8226f8af2e07db4133b87455a
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2018-12-07 10:03:25 -0500

    improve comments for dumpFIRA2csv function

--- dumpFIRAecodes2csv.m
+++ dumpFIRAecodes2csv.m
@@ -1,11 +1,12 @@
 function dumpFIRAecodes2csv(inFilename, outFilename, taskName)
 % writes the FIRA.ecodes matrix to csv file, with column names
 % ARGS:
-%   inFilename      input filename
-%   outFilename     output filename
+%   inFilename      input filename with .mat extension but without full
+%                   path
+%   outFilename     output filename, including full path and extension
 %   taskName        e.g. 'SingleCP_DotsReversal'
 % RETURNS:
-%   Nothing, but creates a file
+%   Nothing, but creates a file with full path outFilename
 
 [~, FIRA] = topsTreeNodeTopNode.getDataFromFile(inFilename, taskName);
 
